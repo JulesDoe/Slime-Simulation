70a7feb034b2debf8efd9f8d6ccfd185084fae57
Jules <>
2023-03-14 18:49:51 +0100

    proof of concept ... wenn ich den movement speed anhand einer texture lokall verändere ergeben sich erkennbar andere muster.

--- Assets/Scripts/Slime/Simulation3.cs
+++ Assets/Scripts/Slime/Simulation3.cs
@@ -4,179 +4,166 @@ using ComputeShaderUtility;
 
 public class Simulation3 : MonoBehaviour
 {
-	public enum SpawnMode { Random, Point, InwardCircle, RandomCircle }
+    public enum SpawnMode { Random, Point, InwardCircle, RandomCircle }
 
-	public MeshRenderer trailMapDebug;
-	public MeshRenderer diffusedTrailMapDebug;
-	public MeshRenderer displayTextureDebug;
-	const int updateKernel = 0;
-	const int diffuseMapKernel = 1;
-	const int colourKernel = 2;
+    public MeshRenderer trailMapDebug;
+    public MeshRenderer diffusedTrailMapDebug;
+    public MeshRenderer displayTextureDebug;
+    public Texture2D angleMap;
+    const int updateKernel = 0;
+    const int diffuseMapKernel = 1;
+	
+    const int colourKernel = 2;
 
-	public ComputeShader compute;
-	public ComputeShader drawAgentsCS;
+    public ComputeShader compute;
+    public ComputeShader drawAgentsCS;
 
-	public SlimeSettings settings;
+    public SlimeSettings settings;
 
-	[Header("Display Settings")]
-	public bool showAgentsOnly;
-	public FilterMode filterMode = FilterMode.Point;
-	public GraphicsFormat format = ComputeHelper.defaultGraphicsFormat;
-
-
-	[SerializeField, HideInInspector] protected RenderTexture trailMap;
-	[SerializeField, HideInInspector] protected RenderTexture diffusedTrailMap;
-	[SerializeField, HideInInspector] protected RenderTexture displayTexture;
-
-	ComputeBuffer agentBuffer;
-	ComputeBuffer settingsBuffer;
-	Texture2D colourMapTexture;
-
-	protected virtual void Start()
-	{
-		Init();
-		trailMapDebug.material.mainTexture = trailMap;
-		diffusedTrailMapDebug.material.mainTexture = diffusedTrailMap;
-		displayTextureDebug.material.mainTexture = displayTexture;
-	}
+    [Header("Display Settings")]
+    public bool showAgentsOnly;
+    public FilterMode filterMode = FilterMode.Point;
+    public GraphicsFormat format = ComputeHelper.defaultGraphicsFormat;
 
 
-	void Init()
-	{
-		// Create render textures
-		ComputeHelper.CreateRenderTexture(ref trailMap, settings.width, settings.height, filterMode, format);
-		ComputeHelper.CreateRenderTexture(ref diffusedTrailMap, settings.width, settings.height, filterMode, format);
-		ComputeHelper.CreateRenderTexture(ref displayTexture, settings.width, settings.height, filterMode, format);
+    [SerializeField, HideInInspector] protected RenderTexture trailMap;
+    [SerializeField, HideInInspector] protected RenderTexture diffusedTrailMap;
+    [SerializeField, HideInInspector] protected RenderTexture displayTexture;
 
-		// Assign textures
-		compute.SetTexture(updateKernel, "TrailMap", trailMap);
-		compute.SetTexture(diffuseMapKernel, "TrailMap", trailMap);
-		compute.SetTexture(diffuseMapKernel, "DiffusedTrailMap", diffusedTrailMap);
-		compute.SetTexture(colourKernel, "ColourMap", displayTexture);
-		compute.SetTexture(colourKernel, "TrailMap", trailMap);
+    ComputeBuffer agentBuffer;
+    ComputeBuffer settingsBuffer;
+    Texture2D colourMapTexture;
 
-		// Create agents with initial positions and angles
-		Agent[] agents = new Agent[settings.numAgents];
-		for (int i = 0; i < agents.Length; i++)
-		{
-			Vector2 centre = new Vector2(settings.width / 2, settings.height / 2);
-			Vector2 startPos = Vector2.zero;
-			float randomAngle = Random.value * Mathf.PI * 2;
-			float angle = 0;
-
-			// if (settings.spawnMode == SpawnMode.Point)
-			// {
-			// 	startPos = centre;
-			// 	angle = randomAngle;
-			// }
-			// else if (settings.spawnMode == SpawnMode.Random)
-			// {
-				startPos = new Vector2(Random.Range(0, settings.width), Random.Range(0, settings.height));
-				angle = randomAngle;
-			// }
-			// else if (settings.spawnMode == SpawnMode.InwardCircle)
-			// {
-			// 	startPos = centre + Random.insideUnitCircle * settings.height * 0.5f;
-			// 	angle = Mathf.Atan2((centre - startPos).normalized.y, (centre - startPos).normalized.x);
-			// }
-			// else if (settings.spawnMode == SpawnMode.RandomCircle)
-			// {
-			// 	startPos = centre + Random.insideUnitCircle * settings.height * 0.15f;
-			// 	angle = randomAngle;
-			// }
-
-			Vector3Int speciesMask;
-			int speciesIndex = 0;
-			int numSpecies = settings.speciesSettings.Length;
-
-			if (numSpecies == 1)
-			{
-				speciesMask = Vector3Int.one;
-			}
-			else
-			{
-				int species = Random.Range(1, numSpecies + 1);
-				speciesIndex = species - 1;
-				speciesMask = new Vector3Int((species == 1) ? 1 : 0, (species == 2) ? 1 : 0, (species == 3) ? 1 : 0);
-			}
-
-			agents[i] = new Agent() { position = startPos, angle = angle, speciesMask = speciesMask, speciesIndex = speciesIndex };
-		}
-
-		ComputeHelper.CreateAndSetBuffer<Agent>(ref agentBuffer, agents, compute, "agents", updateKernel);
-		compute.SetInt("numAgents", settings.numAgents);
-		drawAgentsCS.SetBuffer(0, "agents", agentBuffer);
-		drawAgentsCS.SetInt("numAgents", settings.numAgents);
+    protected virtual void Start()
+    {
+        Init();
+        trailMapDebug.material.mainTexture = trailMap;
+        diffusedTrailMapDebug.material.mainTexture = diffusedTrailMap;
+        displayTextureDebug.material.mainTexture = displayTexture;
+    }
 
 
-		compute.SetInt("width", settings.width);
-		compute.SetInt("height", settings.height);
+    void Init()
+    {
+        // Create render textures
+        ComputeHelper.CreateRenderTexture(ref trailMap, settings.width, settings.height, filterMode, format);
+        ComputeHelper.CreateRenderTexture(ref diffusedTrailMap, settings.width, settings.height, filterMode, format);
+        ComputeHelper.CreateRenderTexture(ref displayTexture, settings.width, settings.height, filterMode, format);
+
+        // Assign textures
+        compute.SetTexture(updateKernel, "TrailMap", trailMap);
+        compute.SetTexture(updateKernel, "AngleMap", angleMap);
+        compute.SetTexture(diffuseMapKernel, "TrailMap", trailMap);
+        compute.SetTexture(diffuseMapKernel, "DiffusedTrailMap", diffusedTrailMap);
+        compute.SetTexture(colourKernel, "ColourMap", displayTexture);
+        compute.SetTexture(colourKernel, "TrailMap", trailMap);
+
+        // Create agents with initial positions and angles
+        Agent[] agents = new Agent[settings.numAgents];
+        for (int i = 0; i < agents.Length; i++)
+        {
+            Vector2 centre = new Vector2(settings.width / 2, settings.height / 2);
+            Vector2 startPos = Vector2.zero;
+            float randomAngle = Random.value * Mathf.PI * 2;
+            float angle = 0;
 
 
-	}
-
-	void FixedUpdate()
-	{
-		for (int i = 0; i < settings.stepsPerFrame; i++)
-		{
-			RunSimulation();
-		}
-	}
-
-	void LateUpdate()
-	{
-		if (showAgentsOnly)
-		{
-			ComputeHelper.ClearRenderTexture(displayTexture);
-
-			drawAgentsCS.SetTexture(0, "TargetTexture", displayTexture);
-			ComputeHelper.Dispatch(drawAgentsCS, settings.numAgents, 1, 1, 0);
-
-		}
-		else
-		{
-			ComputeHelper.Dispatch(compute, settings.width, settings.height, 1, kernelIndex : colourKernel);
-		//	ComputeHelper.CopyRenderTexture(trailMap, displayTexture);
-		}
-	}
-
-	void RunSimulation()
-	{
-		var speciesSettings = settings.speciesSettings;
-		ComputeHelper.CreateStructuredBuffer(ref settingsBuffer, speciesSettings);
-		compute.SetBuffer(updateKernel, "speciesSettings", settingsBuffer);
-		compute.SetBuffer(colourKernel, "speciesSettings", settingsBuffer);
-
-		// Assign settings
-		compute.SetFloat("deltaTime", Time.fixedDeltaTime);
-		compute.SetFloat("time", Time.fixedTime);
-
-		compute.SetFloat("trailWeight", settings.trailWeight);
-		compute.SetFloat("decayRate", settings.decayRate);
-		compute.SetFloat("diffuseRate", settings.diffuseRate);
-		compute.SetInt("numSpecies", speciesSettings.Length);
+            startPos = new Vector2(Random.Range(0, settings.width), Random.Range(0, settings.height));
+            angle = randomAngle;
 
 
-		ComputeHelper.Dispatch(compute, settings.numAgents, 1, 1, kernelIndex: updateKernel);
-		ComputeHelper.Dispatch(compute, settings.width, settings.height, 1, kernelIndex: diffuseMapKernel);
+            Vector3Int speciesMask;
+            int speciesIndex = 0;
+            int numSpecies = settings.speciesSettings.Length;
 
-		ComputeHelper.CopyRenderTexture(diffusedTrailMap, trailMap);
-	}
+            if (numSpecies == 1)
+            {
+                speciesMask = Vector3Int.one;
+            }
+            else
+            {
+                int species = Random.Range(1, numSpecies + 1);
+                speciesIndex = species - 1;
+                speciesMask = new Vector3Int((species == 1) ? 1 : 0, (species == 2) ? 1 : 0, (species == 3) ? 1 : 0);
+            }
 
-	void OnDestroy()
-	{
+            agents[i] = new Agent() { position = startPos, angle = angle, speciesMask = speciesMask, speciesIndex = speciesIndex };
+        }
 
-		ComputeHelper.Release(agentBuffer, settingsBuffer);
-	}
+        ComputeHelper.CreateAndSetBuffer<Agent>(ref agentBuffer, agents, compute, "agents", updateKernel);
+        compute.SetInt("numAgents", settings.numAgents);
+        drawAgentsCS.SetBuffer(0, "agents", agentBuffer);
+        drawAgentsCS.SetInt("numAgents", settings.numAgents);
 
-	public struct Agent
-	{
-		public Vector2 position;
-		public float angle;
-		public Vector3Int speciesMask;
-		int unusedSpeciesChannel;
-		public int speciesIndex;
-	}
+
+        compute.SetInt("width", settings.width);
+        compute.SetInt("height", settings.height);
+
+
+    }
+
+    void FixedUpdate()
+    {
+        for (int i = 0; i < settings.stepsPerFrame; i++)
+        {
+            RunSimulation();
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (showAgentsOnly)
+        {
+            ComputeHelper.ClearRenderTexture(displayTexture);
+
+            drawAgentsCS.SetTexture(0, "TargetTexture", displayTexture);
+            ComputeHelper.Dispatch(drawAgentsCS, settings.numAgents, 1, 1, 0);
+
+        }
+        else
+        {
+            ComputeHelper.Dispatch(compute, settings.width, settings.height, 1, kernelIndex: colourKernel);
+            //	ComputeHelper.CopyRenderTexture(trailMap, displayTexture);
+        }
+    }
+
+    void RunSimulation()
+    {
+        var speciesSettings = settings.speciesSettings;
+        ComputeHelper.CreateStructuredBuffer(ref settingsBuffer, speciesSettings);
+        compute.SetBuffer(updateKernel, "speciesSettings", settingsBuffer);
+        compute.SetBuffer(colourKernel, "speciesSettings", settingsBuffer);
+
+        // Assign settings
+        compute.SetFloat("deltaTime", Time.fixedDeltaTime);
+        compute.SetFloat("time", Time.fixedTime);
+
+        compute.SetFloat("trailWeight", settings.trailWeight);
+        compute.SetFloat("decayRate", settings.decayRate);
+        compute.SetFloat("diffuseRate", settings.diffuseRate);
+        compute.SetInt("numSpecies", speciesSettings.Length);
+
+
+        ComputeHelper.Dispatch(compute, settings.numAgents, 1, 1, kernelIndex: updateKernel);
+        ComputeHelper.Dispatch(compute, settings.width, settings.height, 1, kernelIndex: diffuseMapKernel);
+
+        ComputeHelper.CopyRenderTexture(diffusedTrailMap, trailMap);
+    }
+
+    void OnDestroy()
+    {
+
+        ComputeHelper.Release(agentBuffer, settingsBuffer);
+    }
+
+    public struct Agent
+    {
+        public Vector2 position;
+        public float angle;
+        public Vector3Int speciesMask;
+        int unusedSpeciesChannel;
+        public int speciesIndex;
+    }
 
 
 }
